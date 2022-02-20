4abf15a87fc288f82567e1c6d08371b8192fff8d
Denisa Vítková <it-denisavitkova@stredniskola.net>
2022-02-20 20:24:49 +0100

    Create main.cpp

---
+++ templates/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+typedef long long ll;
+const int M = 1000000007;
+
+void solve() {
+    int n{};
+    std::cin >> n;
+    //solution of the problem goes here
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int T{};
+    std::cin >> T;
+    for(int t{0}; t < T; t++) {
+        solve();
+    }
+    return 0;
+}
